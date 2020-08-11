2c676f6f52023c03e2f8a57afe49c4b1af50bc60
Thanadon Teeratadapong <thanadon.rh@mail.kmutt.ac.th>
2020-08-12 03:26:56 +0700

    Final push

--- lab-01_04.sh
+++ lab-01_04.sh
@@ -1,3 +1,9 @@
 #/bin/bash
 #/usr/bash
 
+wget https://raw.githubusercontent.com/ThanadonTee/csc217/master/lab.tar.gz
+wget https://raw.githubusercontent.com/ThanadonTee/csc217/master/hw.tar.gz
+tar -xvzf lab.tar.gz
+tar -xvzf hw.tar.gz
+rm -rf lab.tar.gz
+rm -rf hw.tar.gz
